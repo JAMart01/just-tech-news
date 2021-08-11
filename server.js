067c414b8e893e74fbeccd8f4032cde9e30044d1
JAMart <jorge.martinez1597@gmail.com>
2021-08-10 21:54:37 -0400

    created api routes and connected Sequelize to the database

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+// Prepares app to parse data 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// MIDDLEWARE
+// turn on routes
+app.use(routes);
+
+
+// turn on connection to db and server 
+sequelize.sync({ force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
