45d4644220c44f6e28ada9fe97c449a89e14d2dd
JAMart <jorge.martinez1597@gmail.com>
2021-08-11 01:51:45 -0400

    created CRUD post routes

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use(routes);
 
 
 // turn on connection to db and server 
-sequelize.sync({ force: false}).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
